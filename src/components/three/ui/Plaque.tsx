c13e7162c01d96d4782b13ad3a7d4a19946b17f4
Jack Kingham <kinghamjack12@gmail.com>
2023-11-21 16:51:28 +0700

    Shorten Attributions

--- src/components/three/ui/Plaque.tsx
+++ src/components/three/ui/Plaque.tsx
@@ -4,8 +4,8 @@ const Plaque = () => {
         <>
             <div className="absolute bottom-0 right-0">
                 <div className=" bg-black/50 rounded-tl-lg px-2 py-1 flex items-center justify-center flex-col sm:flex-row">
-                    <a className="text-[12px] text-white m-2 block" target="_blank" rel="noopener noreferrer" href="https://poly.pizza/u/Poly%20by%20Google">Models by Poly by Google [CC-BY]</a>
-                    <a className="text-[12px] text-white m-2 block" href="https://poly.pizza/m/7qAyGZnerYt" target="_blank" rel="noopener noreferrer">Table by Hunter Paramore [CC-BY] via Poly Pizza</a>
+                    <a className="text-[12px] text-white m-1 block" target="_blank" rel="noopener noreferrer" href="https://poly.pizza/u/Poly%20by%20Google">Models by Poly by Google</a>
+                    <a className="text-[12px] text-white m-1 block" href="https://poly.pizza/m/7qAyGZnerYt" target="_blank" rel="noopener noreferrer">Table by Hunter Paramore</a>
                 </div>
             </div>
 
