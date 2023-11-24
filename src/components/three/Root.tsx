2e3ad94332ff85c1d70ab14380a6d59f782d64aa
Jack Kingham <kinghamjack12@gmail.com>
2023-11-24 10:32:58 +0700

    Update Loader on Habitat

--- src/components/three/Root.tsx
+++ src/components/three/Root.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import Scene from "../models/Scene";
 import { Box, Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
 
 const Root = () => {
 
@@ -12,23 +13,25 @@ const Root = () => {
                 ]
             }}
         >
-            <Environment preset="city" />
-            <OrbitControls
-                autoRotate
-                enablePan={false}
-                enableDamping={false}
-                enableRotate
-                makeDefault
-                maxAzimuthAngle={Math.PI}
-                minPolarAngle={1}
-                maxPolarAngle={1}
-                maxDistance={20}
-                minDistance={15}
-            />
-            <Scene scale={5} />
-            <Box args={[15, 11, 0.5]} position={[0, -0.5, 3]} rotation-x={Math.PI / 2}>
-                <meshStandardMaterial color={'#9e9e9e'} />
-            </Box>
+            <Suspense fallback={null}>
+                <Environment preset="city" />
+                <OrbitControls
+                    autoRotate
+                    enablePan={false}
+                    enableDamping={false}
+                    enableRotate
+                    makeDefault
+                    maxAzimuthAngle={Math.PI}
+                    minPolarAngle={1}
+                    maxPolarAngle={1}
+                    maxDistance={20}
+                    minDistance={15}
+                />
+                <Scene scale={5} />
+                <Box args={[15, 11, 0.5]} position={[0, -0.5, 3]} rotation-x={Math.PI / 2}>
+                    <meshStandardMaterial color={'#9e9e9e'} />
+                </Box>
+            </Suspense>
         </Canvas>
     )
 }
